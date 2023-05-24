802e9d6f476b25785cc0e82bd2bc61e2c0cd71d7
PedroGH10 <p.gonzalez@lapieza.io>
2023-05-24 00:13:20 -0600

    Merge con los nuevos cambios

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'styles/index.css';
-import Home from './home';
 import DiscoverVacancies from './discoverVacancies';
 import HireTalent from './hireTalent';
 import About from './about';
 import Contact from './contact';
 import PoliciesAndPrivacy from "./policiesAndPrivacy";
+import Home from "./home";
 
 function App() {
   const router = createBrowserRouter([
